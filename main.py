2c4adc006a92545cfc5430a6bcf1b3ba5fa8a5d6
Kirill Mikhailov <kicode@mail.ru>
2024-05-09 00:55:56 +0300

    fix

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from messages import send_message_kiwi, send_message_scrouge, send_message_zelenka, send_other
 
-api_id = 22254967
-api_hash = "b877800157e0d8e995d87b04dc3cc977"
-
 app = Client("my_account")
 scheduler = AsyncIOScheduler()
 
